1f7b614000848d9680b926a3baf84dba3400ed76
Tomas Mattia <tomas.mattia@gmail.com>
2011-11-05 16:11:24 -0200

    Added address and contact fields to company views

--- spec/views/companies/_form_spec.rb
+++ spec/views/companies/_form_spec.rb
@@ -11,5 +11,15 @@ describe 'companies/_form.html.haml' do
 
   it { should have_field(t('simple_form.labels.name')) }
   it { should have_field(t('simple_form.labels.email')) }
+  it { should have_field(t('simple_form.labels.company_name')) }
+  it { should have_field(t('simple_form.labels.cnpj')) }
+  it { should have_field(t('simple_form.labels.street')) }
+  it { should have_field(t('simple_form.labels.zip_code')) }
+  it { should have_field(t('simple_form.labels.city')) }
+  it { should have_field(t('simple_form.labels.state')) }
+  it { should have_field(t('simple_form.labels.phone')) }
+  it { should have_field(t('simple_form.labels.fax')) }
+  it { should have_field(t('simple_form.labels.msn')) }
+  it { should have_field(t('simple_form.labels.skype')) }
 
 end
