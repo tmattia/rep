00f481d4942e0fbfa64c0be3e0b9b6e213b2a05a
Tomas Mattia <tomas.mattia@gmail.com>
2011-11-05 02:49:37 -0200

    Added validations and specs to companies address and contact fields

--- app/models/company.rb
+++ app/models/company.rb
@@ -12,4 +12,24 @@ class Company < ActiveRecord::Base
                     :email => :true,
                     :length => { :maximum => 255 }
 
+  validates :company_name, :length => { :maximum => 255 }
+
+  validates :cnpj, :length => { :within => 14..18 }, :allow_blank => true
+
+  validates :street, :length => { :maximum => 255 }
+
+  validates :zip_code, :length => { :within => 8..10 }, :allow_blank => true
+
+  validates :city, :length => { :maximum => 255 }
+
+  validates :state, :length => { :is => 2 }, :allow_blank => true
+
+  validates :phone, :length => { :maximum => 255 }
+
+  validates :fax, :length => { :maximum => 255 }
+
+  validates :msn, :length => { :maximum => 255 }
+
+  validates :skype, :length => { :maximum => 255 }
+
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -17,4 +17,30 @@ describe Company do
   it { should_not allow_value("invalid@email").for(:email) }
   it { should allow_value("example@mail.com").for(:email) }
 
+  it { should ensure_length_of(:company_name).is_at_most(255) }
+
+  it { should allow_value("").for(:cnpj) }
+  it { should ensure_length_of(:cnpj).is_at_least(14) }
+  it { should ensure_length_of(:cnpj).is_at_most(18) }
+
+  it { should ensure_length_of(:street).is_at_most(255) }
+
+  it { should allow_value("").for(:zip_code) }
+  it { should ensure_length_of(:zip_code).is_at_least(8) }
+  it { should ensure_length_of(:zip_code).is_at_most(10) }
+
+  it { should ensure_length_of(:city).is_at_most(255) }
+
+  it { should allow_value("").for(:state) }
+  it { should ensure_length_of(:state).is_equal_to(2) }
+
+  it { should ensure_length_of(:phone).is_at_most(255) }
+
+  it { should ensure_length_of(:fax).is_at_most(255) }
+
+  it { should ensure_length_of(:msn).is_at_most(255) }
+
+  it { should ensure_length_of(:skype).is_at_most(255) }
+
+
 end
