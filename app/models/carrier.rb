c32ca610dd35eba65dde6c8d759fe344d0af8bb9
Tomas Mattia <tomas.mattia@gmail.com>
2011-12-06 15:10:47 -0200

    Added has_many relationship from carriers to orders

--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -2,6 +2,8 @@ class Carrier < ActiveRecord::Base
 
   default_scope :order => 'name ASC'
 
+  has_many :orders
+
   validates :name, :presence => true,
                    :uniqueness => true,
                    :length => { :maximum => 255 }
--- spec/models/carrier_spec.rb
+++ spec/models/carrier_spec.rb
@@ -4,6 +4,8 @@ describe Carrier do
 
   subject { Factory(:carrier) }
 
+  it { should have_many(:orders) }
+
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:name) }
   it { should ensure_length_of(:name).is_at_most(255) }
